fec09be42e3745d3b6c98e5574ebb8072f85fddb
mlheur <mlheur@gmail.com>
2025-12-16 13:50:51 -0800

    rewritten to stop writing to db.xml

--- PlayLang_item.lua
+++ PlayLang_item.lua
@@ -1,20 +1,24 @@
 function onInit()
-    LangNode = getDatabaseNode()
-    sLang = LangNode.getValue()
-    sPlayers = ""
-    for _,nPS in ipairs(PartyManager.getPartyNodes()) do
-        nChar = PartyManager.mapPStoChar(nPS)
-        sCharName = DB.getChild(nChar,"name").getValue()
-        for _,nCharLang in pairs(DB.getChildren(nChar,"languagelist")) do
-            sKnownLangs = StringManager.trim(DB.getValue(nCharLang, "name", ""))
-            if ( sKnownLangs == sLang ) then
-                if sPlayers == "" then
-                    sPlayers = sCharName
-                else
-                    sPlayers = sPlayers .. "," .. sCharName
+    nLanguage = window.getDatabaseNode()
+    sLang = DB.getValue(nLanguage,"name")
+    sPlayers = "<none>"
+    if sLang then
+        sPlayers = ""
+        for _,nPS in ipairs(PartyManager.getPartyNodes()) do
+            nChar = PartyManager.mapPStoChar(nPS)
+            sCharName = DB.getValue(nChar,"name","")
+            aCharNameParts = StringManager.splitWords(sCharName)
+            for _,nCharLang in ipairs(DB.getChildList(nChar,"languagelist")) do
+                sKnownLangs = StringManager.trim(DB.getValue(nCharLang, "name", ""))
+                if ( sKnownLangs == sLang ) then
+                    if sPlayers == "" then
+                        sPlayers = aCharNameParts[1]
+                    else
+                        sPlayers = sPlayers .. ", " .. aCharNameParts[1]
+                    end
                 end
             end
         end
     end
-    LangNode.getParent().getChild("PlayLang_players").setValue(sPlayers)
+    window.speakers.setValue(sPlayers)
 end
